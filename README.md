294178a7a5125cac2202be8ffd34404efae5b35b
MatrixAwakens69 <ponnuponnanna94@gmail.com>
2024-03-24 10:54:46 +0530

    Update code formatting and fix minor bugs

---
+++ README.md
@@ -0,0 +1,7 @@
+# DBMS Project
+
+## Members:
+
+1. Ponnanna A H (21CS02003)
+2. Shubh Modi (21CS01073)
+3. Tejas Singh (21CS01011)
---
+++ backend/server.js
@@ -0,0 +1,79 @@
+import express from "express";
+import dotenv from "dotenv";
+import mysql from "mysql2";
+
+dotenv.config();
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASS,
+  database: process.env.DB_SCHEMA,
+});
+
+db.connect(() => {
+  try {
+    console.log("Connected to MySQL database");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
+
+app.get("/", (req, res) => {
+  try {
+    res.json("Hello, World!");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/patients", (req, res) => {
+  const query = "SELECT * FROM patients";
+
+  db.query(query, (error, data) => {
+    try {
+      res.json(data);
+      //   console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+app.post("/add-patient", (req, res) => {
+  const {
+    first_name,
+    last_name,
+    date_of_birth,
+    gender,
+    address,
+    phone_number,
+  } = req.body;
+
+  const query =
+    "INSERT INTO patients (first_name, last_name, date_of_birth, gender, address, phone_number) VALUES (?, ? ,?, ?, ?, ?)";
+
+  db.query(
+    query,
+    [first_name, last_name, date_of_birth, gender, address, phone_number],
+    (error, data) => {
+      if (error) {
+        console.error("Error inserting patient:", error);
+        return res.status(500).json({
+          error: "Internal Server Error",
+        });
+      }
+      res.status(201).json({
+        message: "Patient inserted successfully.",
+      });
+    }
+  );
+});
