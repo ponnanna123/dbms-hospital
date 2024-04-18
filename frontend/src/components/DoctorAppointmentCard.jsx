94df584e8a9c4af9ade64a1f6d70b7cddc8da568
ponnanna123 <21cs02003@iitbbs.ac.in>
2024-04-18 08:24:19 +0530

    Complete doctor dashboard

---
+++ frontend/src/components/DoctorAppointmentCard.jsx
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+const DoctorAppointmentCard = ({ appointment }) => {
+  const dateTime = new Date(appointment.appointment_datetime);
+  const date = dateTime.toLocaleDateString();
+  const time = dateTime.toLocaleTimeString();
+
+  const handleUpdateStatus = async (appt_id, status) => {
+    try {
+      const response = await axios.put(
+        `/api/appointments/update/status/${appt_id}`,
+        { status }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log("Failed to delete appointment", error);
+    }
+  };
+
+  return (
+    <div className="rounded overflow-hidden shadow-lg">
+      <img
+        className="w-full h-64 object-cover"
+        src={appointment.image}
+        alt="Background"
+      />
+      <div className="flex justify-between bg-white shadow-lg rounded-lg p-6 mb-6">
+        <div>
+          <h2 className="text-2xl font-bold text-gray-800 mb-2">
+            {appointment.gender === "M" ? "Mr" : "Mrs"}.{" "}
+            {appointment.first_name} {appointment.last_name}
+          </h2>
+          <p className="text-gray-600">
+            <span className="font-bold">Date:</span> {date}
+          </p>
+          <p className="text-gray-600">
+            <span className="font-bold">Time:</span> {time}
+          </p>
+        </div>
+        <div className="text-gray-600">
+          <button
+            hidden={
+              appointment.status === "Canceled" ||
+              appointment.status === "Completed"
+            }
+            onClick={() =>
+              handleUpdateStatus(appointment.appointment_id, "Canceled")
+            }
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mb-4"
+          >
+            Cancel
+          </button>
+          <button
+            hidden={
+              appointment.status === "Canceled" ||
+              appointment.status === "Completed"
+            }
+            onClick={() =>
+              handleUpdateStatus(appointment.appointment_id, "Completed")
+            }
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+          >
+            Finish
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorAppointmentCard;
--- frontend/src/pages/DoctorDashboard.jsx
+++ frontend/src/pages/DoctorDashboard.jsx
@@ -1,13 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import DoctorAppointmentCard from "../components/DoctorAppointmentCard";
 
 const DoctorDashboard = () => {
   const [appointments, setAppointments] = useState([]);
   const { currentUser } = useSelector((state) => state.user);
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAppointments = async () => {
@@ -24,79 +22,51 @@ const DoctorDashboard = () => {
     fetchAppointments();
   }, [appointments]);
 
-  const handleUpdate = async (appt_id, status) => {
-    try {
-      const response = await axios.put(
-        `/api/appointments/update/status/${appt_id}`,
-        { status }
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.log("Failed to delete appointment", error);
-    }
-  };
+  const pendingAppointments = appointments.filter(
+    (appt) => appt.status === "Scheduled"
+  );
+  const completedAppointments = appointments.filter(
+    (appt) => appt.status === "Completed"
+  );
 
   return (
     <div className="flex flex-col items-center bg-green-200 pt-36 lg:pl-16 lg:pr-16 xl:pl-24 xl:pr-24 pl-12 pr-12">
       <div className="flex justify-between w-full px-4 mb-20 mt-4 transform -translate-y-10">
         <h3 className="text-4xl font-bold">List of appointments:</h3>
       </div>
-      {appointments.length > 0 ? (
+      <h3 className="text-4xl font-semibold mb-10">Pending</h3>
+      {pendingAppointments.length > 0 ? (
         <div className="flex flex-wrap justify-start w-full px-4 mb-40">
-          {appointments.map((appointment) => {
-            const dateTime = new Date(appointment.appointment_datetime);
-            const date = dateTime.toLocaleDateString();
-            const time = dateTime.toLocaleTimeString();
-            return (
-              <div
-                key={appointment.appointment_id}
-                className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 p-4"
-              >
-                <div className="rounded overflow-hidden shadow-lg">
-                  <img
-                    className="w-full h-64 object-cover"
-                    src="../../images/blood-drop.png"
-                    alt="logo"
-                  />
-                  <div className="flex justify-between bg-white shadow-lg rounded-lg p-6 mb-6">
-                    <div className="px-6 py-4">
-                      <div className="font-bold text-xl mb-2">
-                        {appointment.first_name} {appointment.last_name}
-                      </div>
-                      <p className="text-gray-600">
-                        <span className="font-bold">Status:</span>
-                        <br />
-                        {appointment.status}
-                      </p>
-                      <p className="text-gray-600 text-base">Date: {date}</p>
-                      <p className="text-gray-600 text-base">Time: {time}</p>
-                    </div>
-                    <div className="px-6 py-4 flex flex-col gap-y-6">
-                      <button
-                        onClick={() =>
-                          handleUpdate(appointment.appointment_id, "Completed")
-                        }
-                        className="px-4 py-2 bg-green-500 text-white rounded-full hover:bg-green-700"
-                      >
-                        Complete
-                      </button>
-                      <button
-                        onClick={() =>
-                          handleUpdate(appointment.appointment_id, "Canceled")
-                        }
-                        className="px-4 py-2 bg-red-500 text-white rounded-full hover:bg-red-700"
-                      >
-                        Cancel
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+          {pendingAppointments.map((appt) => (
+            <div
+              key={appt.appointment_id}
+              className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 p-4"
+            >
+              <DoctorAppointmentCard appointment={appt} />
+            </div>
+          ))}
         </div>
       ) : (
-        <h3 className="text-2xl font-bold">No appointments scheduled</h3>
+        <h3 className="text-5xl font-bold text-center mt-80 mb-80">
+          No pending appointments
+        </h3>
+      )}
+      <h3 className="text-4xl font-semibold mb-10">Completed</h3>
+      {completedAppointments.length > 0 ? (
+        <div className="flex flex-wrap justify-start w-full px-4 mb-40">
+          {completedAppointments.map((appt) => (
+            <div
+              key={appt.appointment_id}
+              className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 p-4"
+            >
+              <DoctorAppointmentCard appointment={appt} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h3 className="text-5xl font-bold text-center mt-80 mb-80">
+          No completed appointments
+        </h3>
       )}
     </div>
   );
